6c2d1a598ed9115cca15090f16eb057c5d8de9c0
Guilherme <guilherme.dsl@hotmail.com>
2024-09-29 17:06:42 -0300

    Fronted e Backend conectados e funcionando corretamente agora.

--- backend/ef_crud_api/obj/Debug/net8.0/ef_crud_api.AssemblyInfo.cs
+++ backend/ef_crud_api/obj/Debug/net8.0/ef_crud_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ef_crud_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b582e1dc674de08554881c749eeb506b47fa71c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e368264145f0425aaa96074a8fe6db949dff7c1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ef_crud_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ef_crud_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
